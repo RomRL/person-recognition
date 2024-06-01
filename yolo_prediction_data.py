21f948416f5aaf2464e1e79fad2ee514aa41c2c5
Rom Harel <109737626+RomRL@users.noreply.github.com>
2024-06-01 21:08:32 +0300

    Update yolo_prediction_data.py

--- yolo_prediction_data.py
+++ yolo_prediction_data.py
@@ -112,7 +112,7 @@ if __name__ == "__main__":
     frame_detections: list[list[Detection]] = detector.process_video("videoplayback.mp4", target_class_id='person')
 
     for frame_idx, detections in enumerate(frame_detections):
-        # For debug
+        # For debug - 
         # fig, ax = plt.subplots(1)
         # ax.imshow(cv2.cvtColor(detections[0].image_patch, cv2.COLOR_BGR2RGB))
         print(f"\nFrame {frame_idx + 1}: {len(detections)} detections")
